e390a80bd8b3d47a6d01f6cfe6fbda108d838944
a-sv <runtime.rnt@gmail.com>
2018-04-04 15:55:38 +0300

    common: added namespace for units

--- common/units.h
+++ common/units.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <lmcu/device>
 
+namespace lmcu {
+
 constexpr auto operator"" _MHz(long double x) { return static_cast<uint32_t>(x * 1e6); }
 constexpr auto operator"" _MHz(unsigned long long int x) { return static_cast<uint32_t>(x * 1e6); }
 constexpr auto operator"" _kHz(long double x) { return static_cast<uint32_t>(x * 1e3); }
 constexpr auto operator"" _kHz(unsigned long long int x) { return static_cast<uint32_t>(x * 1e3); }
+
+} // namespace lmcu
