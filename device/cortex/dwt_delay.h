0061cdddd8d9378c9fad2bb197aed223fc386124
a-sv <runtime.rnt@gmail.com>
2018-04-05 13:54:49 +0300

    more readable constant in delay function

--- device/cortex/dwt_delay.h
+++ device/cortex/dwt_delay.h
@@ -7,7 +7,7 @@ namespace delay {
 template<uint32_t _us>
 void us()
 {
-  const uint32_t cyccnt = (rcc::detail::system_clock / 1000000) * _us;
+  const uint32_t cyccnt = (rcc::detail::system_clock / 1_MHz) * _us;
 
   CoreDebug->DEMCR |=  CoreDebug_DEMCR_TRCENA_Msk; // enable DWT
   DWT->CTRL        &= ~DWT_CTRL_CYCCNTENA_Msk; // stop DWT
