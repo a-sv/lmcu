32c248f83862bf68a1418d35056afe1a48260dc1
a-sv <runtime.rnt@gmail.com>
2018-04-17 11:31:51 +0300

    cortex: added new delay functions

---
+++ common/def.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define lmcu_force_inline static inline __attribute__((always_inline))
--- device/cortex/dwt_delay.h
+++ device/cortex/dwt_delay.h
@@ -1,32 +1,67 @@
 #pragma once
 #include <lmcu/rcc>
+#include "../../common/def.h"
 
 namespace lmcu {
 namespace delay {
 
 /**
- * @brief Delay with microsecond resolution.
- * @param _us: delay in microsecond
- *
- * @note  IMPORTANT: max_delay = (0xffffffff / f_cpu) * 1e6
+ * @brief Constant delay in cycles.
+ * @param _cyc: cycles to wait
 */
-template<uint32_t _us>
-void us()
+template<uint32_t _cyc>
+lmcu_force_inline void cyc()
 {
-  const uint32_t delay = ((rcc::detail::system_clock / 1_MHz) * _us) - 8;
-  const uint32_t ovf   = (0xffffffff - DWT->CYCCNT) + 1;
-  while(delay > (DWT->CYCCNT + ovf))
+  const uint32_t ovf = (0xffffffff - DWT->CYCCNT) + 1;
+  while(_cyc > (DWT->CYCCNT + ovf))
     ;
 }
 
 /**
- * @brief Delay with millisecond resolution.
- * @param _us: delay in millisecond
+ * @brief Dynamic delay in cycles.
+ * @param cyc: cycles to wait
 */
-template<uint32_t _ms>
-void ms()
+lmcu_force_inline void cyc(const uint32_t cyc)
 {
-  for(uint32_t n = 0; n < _ms; n++) { us<1000>(); }
+  const uint32_t ovf = (0xffffffff - DWT->CYCCNT) + 1;
+  while(cyc > (DWT->CYCCNT + ovf))
+    ;
+}
+
+/**
+ * @brief Constant delay in microseconds.
+ * @param _us: microsecond to wait
+ *
+ * @note  More precisely than the dynamic version.
+ *        IMPORTANT: max_delay = (0xffffffff / f_cpu) * 1e6
+*/
+template<uint32_t _us>
+lmcu_force_inline void us() { cyc((rcc::detail::system_clock / 1_MHz) * _us - 8); }
+
+/**
+ * @brief Dynamic delay in microseconds.
+ * @param us: microseconds to wait
+ *
+ * @note  IMPORTANT: for max_delay see constant version.
+*/
+lmcu_force_inline void us(const uint32_t us) { cyc((rcc::detail::system_clock / 1_MHz) * us - 8); }
+
+/**
+ * @brief Delay in millisecond.
+ * @param ms: milliseconds to wait
+*/
+lmcu_force_inline void ms(const uint32_t ms)
+{
+  for(uint32_t n = 0; n < ms; n++) { us<1000>(); }
+}
+
+/**
+ * @brief Delay in seconds.
+ * @param sec: seconds to wait
+*/
+lmcu_force_inline void sec(const uint32_t sec)
+{
+  for(uint32_t n = 0; n < sec; n++) { us<1000000>(); }
 }
 
 } // namespace delay
