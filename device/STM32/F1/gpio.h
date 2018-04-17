0b98bb756fba188efc3d9eaf306feb880ef2c1d5
a-sv <runtime.rnt@gmail.com>
2018-04-17 14:14:45 +0300

    STM32F1xx: gpio - enable alternate function clock by default

--- device/STM32/F1/gpio.h
+++ device/STM32/F1/gpio.h
@@ -7,7 +7,7 @@ namespace gpio {
 #include "detail/gpio.h"
 
 template<port ...args>
-void enable(bool afio_on = false) { detail::enable<args...>(afio_on); }
+void enable(bool afio_on = true) { detail::enable<args...>(afio_on); }
 
 template<port ...args>
 void disable(bool afio_off = true) { detail::disable<args...>(afio_off); }
