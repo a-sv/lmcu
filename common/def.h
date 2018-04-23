d1d40e348ac5451a76f9db73b3dc7d42af2b5559
a-sv <runtime.rnt@gmail.com>
2018-04-23 23:21:41 +0300

    fix compile error with -O0 optimization

--- common/def.h
+++ common/def.h
@@ -1,3 +1,7 @@
 #pragma once
 
-#define lmcu_force_inline static inline __attribute__((always_inline))
+#if defined(__NO_INLINE__)
+  #define lmcu_force_inline inline
+#else
+  #define lmcu_force_inline inline __attribute__((always_inline))
+#endif
