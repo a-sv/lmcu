84e3eb884b40ce5cbb3a19853f0e367fc83f2a28
a-sv <runtime.rnt@gmail.com>
2018-07-31 21:45:13 +0300

    added lmcu_defer for code run on scope exit

---
+++ common/defer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+template<typename T>
+class __defer_9ec0ab5f
+{
+  __defer_9ec0ab5f(const __defer_9ec0ab5f&) = delete;
+  __defer_9ec0ab5f &operator =(const __defer_9ec0ab5f&) = delete;
+public:
+  __defer_9ec0ab5f(T&& fn): fn_(fn) { }
+  ~__defer_9ec0ab5f() { fn_(); }
+private:
+  T fn_;
+};
+
+#define __defer_9ec0ab5f_1(line) __defer_9ec0ab5f_obj_##line
+#define __defer_9ec0ab5f_2(line) __defer_9ec0ab5f_1(line)
+
+#define lmcu_defer(fn) __defer_9ec0ab5f __defer_9ec0ab5f_2(__LINE__)(fn)
