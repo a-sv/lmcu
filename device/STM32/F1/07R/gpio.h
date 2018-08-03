8728f4406a7b45b81740704c6b51bb0651b10314
a-sv <runtime.rnt@gmail.com>
2018-08-03 10:07:45 +0300

    STM32F107R: added gpio support

---
+++ device/STM32/F1/07R/gpio.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "../gpio.h"
+
+namespace lmcu {
+namespace gpio {
+
+lmcu_gpio_pin(A,0, );
+lmcu_gpio_pin(A,1, );
+lmcu_gpio_pin(A,2, );
+lmcu_gpio_pin(A,3, );
+lmcu_gpio_pin(A,4, );
+lmcu_gpio_pin(A,5, );
+lmcu_gpio_pin(A,6, );
+lmcu_gpio_pin(A,7, );
+lmcu_gpio_pin(A,8, );
+lmcu_gpio_pin(A,9, );
+lmcu_gpio_pin(A,10,);
+lmcu_gpio_pin(A,11,);
+lmcu_gpio_pin(A,12,);
+lmcu_gpio_pin(A,13,);
+lmcu_gpio_pin(A,14,);
+lmcu_gpio_pin(A,15,);
+
+lmcu_gpio_pin(B,0, );
+lmcu_gpio_pin(B,1, );
+lmcu_gpio_pin(B,2, );
+lmcu_gpio_pin(B,3, );
+lmcu_gpio_pin(B,4, );
+lmcu_gpio_pin(B,5, );
+lmcu_gpio_pin(B,6, );
+lmcu_gpio_pin(B,7, );
+lmcu_gpio_pin(B,8, );
+lmcu_gpio_pin(B,9, );
+lmcu_gpio_pin(B,10,);
+lmcu_gpio_pin(B,11,);
+lmcu_gpio_pin(B,12,);
+lmcu_gpio_pin(B,13,);
+lmcu_gpio_pin(B,14,);
+lmcu_gpio_pin(B,15,);
+
+lmcu_gpio_pin(C,0, );
+lmcu_gpio_pin(C,1, );
+lmcu_gpio_pin(C,2, );
+lmcu_gpio_pin(C,3, );
+lmcu_gpio_pin(C,4, );
+lmcu_gpio_pin(C,5, );
+lmcu_gpio_pin(C,6, );
+lmcu_gpio_pin(C,7, );
+lmcu_gpio_pin(C,8, );
+lmcu_gpio_pin(C,9, );
+lmcu_gpio_pin(C,10,);
+lmcu_gpio_pin(C,11,);
+lmcu_gpio_pin(C,12,);
+lmcu_gpio_pin(C,13,);
+lmcu_gpio_pin(C,14,);
+lmcu_gpio_pin(C,15,);
+
+lmcu_gpio_pin(D,2, );
+
+
+} // namespace gpio
+} // namespace lmcu
