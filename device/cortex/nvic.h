c47b1c8d87296a2b3d0f04c8294d2e5ef79d0950
a-sv <runtime.rnt@gmail.com>
2018-08-08 11:00:53 +0300

    fixed parameter type for nvic::enable_irq

--- device/cortex/nvic.h
+++ device/cortex/nvic.h
@@ -14,7 +14,7 @@ struct irq
   static constexpr auto sub_prio     = _sub_prio;
 };
 
-template<typename _irq, uint32_t _n>
+template<typename _irq, IRQn_Type _n>
 void enable_irq()
 {
   NVIC_SetPriority(_n, NVIC_EncodePriority(_irq::prio_group, _irq::preempt_prio, _irq::sub_prio));
