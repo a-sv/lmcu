584d39868b0f1b5d7df773dff1a47a7757799991
a-sv <runtime.rnt@gmail.com>
2018-08-08 10:56:53 +0300

    added warning for no_inline; deleted operator == for flags

--- common/def.h
+++ common/def.h
@@ -2,6 +2,7 @@
 
 #if defined(__NO_INLINE__)
   #define lmcu_force_inline inline
+  #pragma message("warning: inlining disabled, this may cause problems with lmcu correct working!")
 #else
   #define lmcu_force_inline inline __attribute__((always_inline))
 #endif
@@ -11,4 +12,6 @@
   { return static_cast<T>( static_cast<uint32_t>(lhs) | static_cast<uint32_t>(rhs) ); } \
   \
   constexpr bool operator &(T lhs, T rhs) \
-  { return (static_cast<uint32_t>(lhs) & static_cast<uint32_t>(rhs)) != 0; }
+  { return (static_cast<uint32_t>(lhs) & static_cast<uint32_t>(rhs)) != 0; } \
+  \
+  constexpr bool operator ==(T lhs, T rhs) = delete; // use only operator & for flags checking
