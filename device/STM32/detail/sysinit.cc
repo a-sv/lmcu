6a874b62773e8a48def4ab2fe267cdff28cfb4aa
a-sv <runtime.rnt@gmail.com>
2018-08-28 22:35:15 +0300

    added NVIC config to sysinit

--- device/STM32/detail/sysinit.cc
+++ device/STM32/detail/sysinit.cc
@@ -15,6 +15,26 @@ void sysinit_low_freq_stage()
   // enable cortex DWT cycles counter
   CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
   DWT->CTRL        |= DWT_CTRL_CYCCNTENA_Msk;
+
+  //
+  // NVIC config
+  //
+
+  NVIC_SetPriorityGrouping(3);
+  auto ip = NVIC_EncodePriority(3, 0, 0);
+
+  // MemoryManagement_IRQn interrupt configuration
+  NVIC_SetPriority(MemoryManagement_IRQn, ip);
+  // BusFault_IRQn interrupt configuration
+  NVIC_SetPriority(BusFault_IRQn, ip);
+  // UsageFault_IRQn interrupt configuration
+  NVIC_SetPriority(UsageFault_IRQn, ip);
+  // SVCall_IRQn interrupt configuration
+  NVIC_SetPriority(SVCall_IRQn, ip);
+  // DebugMonitor_IRQn interrupt configuration
+  NVIC_SetPriority(DebugMonitor_IRQn, ip);
+  // PendSV_IRQn interrupt configuration
+  NVIC_SetPriority(PendSV_IRQn, ip);
 }
 
 } // namespace lmcu::detail
