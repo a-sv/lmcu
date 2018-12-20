6be35a6a3e55125ed718f6cdc624f4d17aaf43ee
a-sv <runtime.rnt@gmail.com>
2018-12-20 16:25:32 +0300

    added new units

--- common/units.h
+++ common/units.h
@@ -8,4 +8,18 @@ constexpr auto operator"" _MHz(unsigned long long int x) { return static_cast<ui
 constexpr auto operator"" _kHz(long double x) { return static_cast<uint32_t>(x * 1e3); }
 constexpr auto operator"" _kHz(unsigned long long int x) { return static_cast<uint32_t>(x * 1e3); }
 
+constexpr auto operator"" _Mbyte(long double x) { return static_cast<uint64_t>(x * 1024 * 1024); }
+
+constexpr auto operator"" _Mbyte(unsigned long long int x)
+{
+  return static_cast<uint64_t>(x * 1024 * 1024);
+}
+
+constexpr auto operator"" _Kbyte(long double x) { return static_cast<uint64_t>(x * 1024); }
+
+constexpr auto operator"" _Kbyte(unsigned long long int x)
+{
+  return static_cast<uint64_t>(x * 1024);
+}
+
 } // namespace lmcu
