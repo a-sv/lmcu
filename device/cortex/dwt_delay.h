1ed396e7a59c514565055a83bd62ef5e28cf1a0f
a-sv <runtime.rnt@gmail.com>
2018-12-25 17:34:08 +0300

    delay: added timeout support for blocking functions

--- device/cortex/dwt_delay.h
+++ device/cortex/dwt_delay.h
@@ -17,13 +17,45 @@ lmcu_force_inline void cyc(const uint32_t cyc)
 }
 
 /**
+ * @brief Delay in cycles with timeout.
+ * @param cyc: cycles to wait
+ * @param t  : timeout
+ *
+ * @return 'false' if timeout occured, else 'true'
+*/
+lmcu_force_inline bool cyc(const uint32_t cyc, const expirable &t)
+{
+  const uint32_t ovf = start();
+  while(cyc > (DWT->CYCCNT + ovf)) {
+    if(t.expired()) { return false; }
+  }
+  return true;
+}
+
+/**
  * @brief Delay in nanoseconds.
  * @param ns: nanoseconds to wait
 */
-lmcu_force_inline void ns(const uint64_t ns)
+lmcu_force_inline void ns(const uint32_t ns)
 {
   uint32_t n = to_cycles<units::ns>(ns);
   if(!__builtin_usubl_overflow(n, 8, &n)) { cyc(n); }
+}
+
+/**
+ * @brief Delay in nanoseconds with timeout.
+ * @param ns: nanoseconds to wait
+ * @param t : timeout
+ *
+ * @return 'false' if timeout occured, else 'true'
+*/
+lmcu_force_inline bool ns(const uint32_t ns, const expirable &t)
+{
+  uint32_t n = to_cycles<units::ns>(ns);
+  if(!__builtin_usubl_overflow(n, 8, &n)) {
+    if(!cyc(n, t)) { return false; }
+  }
+  return true;
 }
 
 /**
@@ -39,6 +71,22 @@ lmcu_force_inline void us(const uint32_t us)
 }
 
 /**
+ * @brief Delay in microseconds with timeout.
+ * @param us: microsecond to wait
+ * @param t : timeout
+ *
+ * @return 'false' if timeout occured, else 'true'
+*/
+lmcu_force_inline bool us(const uint32_t us, const expirable &t)
+{
+  uint32_t n = to_cycles<units::us>(us);
+  if(!__builtin_usubl_overflow(n, 8, &n)) {
+    if(!cyc(n, t)) { return false; }
+  }
+  return true;
+}
+
+/**
  * @brief Delay in millisecond.
  * @param ms: milliseconds to wait
 */
@@ -48,12 +96,42 @@ lmcu_force_inline void ms(const uint32_t ms)
 }
 
 /**
+ * @brief Delay in millisecond with timeout.
+ * @param ms: milliseconds to wait
+ * @param t : timeout
+ *
+ * @return 'false' if timeout occured, else 'true'
+*/
+lmcu_force_inline bool ms(const uint32_t ms, const expirable &t)
+{
+  for(uint32_t n = 0; n < ms; n++) {
+    if(!us(1000, t)) { return false; }
+  }
+  return true;
+}
+
+/**
  * @brief Delay in seconds.
  * @param sec: seconds to wait
 */
 lmcu_force_inline void sec(const uint32_t sec)
 {
   for(uint32_t n = 0; n < sec; n++) { us(1000000); }
+}
+
+/**
+ * @brief Delay in seconds with timeout.
+ * @param sec: seconds to wait
+ * @param t : timeout
+ *
+ * @return 'false' if timeout occured, else 'true'
+*/
+lmcu_force_inline bool sec(const uint32_t sec, const expirable &t)
+{
+  for(uint32_t n = 0; n < sec; n++) {
+    if(!us(1000000, t)) { return false; }
+  }
+  return true;
 }
 
 namespace dwt {
