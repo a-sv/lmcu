2236efdf3d9e5d1c5ec0ff9d6933bdf465c26aca
a-sv <runtime.rnt@gmail.com>
2018-12-25 13:51:07 +0300

    delay: added 'ns' delay; fix overflow error; code refactor;

--- common/delay/timer.h
+++ common/delay/timer.h
@@ -1,9 +1,30 @@
 #pragma once
-#include <lmcu/device>
+#include <lmcu/rcc>
 #include "expirable.h"
 #include "../def.h"
 
 namespace lmcu::delay {
+
+enum class units { cyc, ns, us, ms, sec };
+
+template<units _units>
+uint64_t to_cycles(uint64_t val)
+{
+  static_assert(_units != units::cyc, "invalid param: units::cyc");
+
+  const uint64_t cyc_per_us = rcc::system_clock() / 1_MHz;
+
+  if constexpr(_units == units::ns) {
+    const uint32_t ns_per_cycle = ((10000 / cyc_per_us) + 5) / 10;
+    return (((val * 10) / ns_per_cycle) + 5) / 10;
+  }
+
+  if constexpr(_units == units::us) { return cyc_per_us * val; }
+
+  if constexpr(_units == units::ms) { return cyc_per_us * val * 1000; }
+
+  return cyc_per_us * val * 1000000;
+}
 
 class timer : public expirable
 {
--- device/cortex/dwt_delay.h
+++ device/cortex/dwt_delay.h
@@ -1,29 +1,15 @@
 #pragma once
-#include <lmcu/rcc>
-#include <lmcu/common>
 #include <lmcu/delay>
 
 namespace lmcu::delay {
 
-lmcu_force_inline uint32_t start() { return (0xffffffff - DWT->CYCCNT) + 1; }
+static inline uint32_t start() { return (0xffffffff - DWT->CYCCNT) + 1; }
 
 /**
- * @brief Constant delay in cycles.
- * @param _cyc: cycles to wait
-*/
-template<uint32_t _cyc>
-lmcu_force_inline void cyc()
-{
-  const uint32_t ovf = start();
-  while(_cyc > (DWT->CYCCNT + ovf))
-    ;
-}
-
-/**
- * @brief Dynamic delay in cycles.
+ * @brief Delay in cycles.
  * @param cyc: cycles to wait
 */
-lmcu_force_inline void cyc(const uint32_t cyc)
+static inline void cyc(const uint32_t cyc)
 {
   const uint32_t ovf = start();
   while(cyc > (DWT->CYCCNT + ovf))
@@ -31,39 +17,39 @@ lmcu_force_inline void cyc(const uint32_t cyc)
 }
 
 /**
- * @brief Constant delay in microseconds.
- * @param _us: microsecond to wait
- *
- * @note  More precisely than the dynamic version.
- *        IMPORTANT: max_delay = (0xffffffff / f_cpu) * 1e6
+ * @brief Delay in nanoseconds.
+ * @param ns: nanoseconds to wait
 */
-template<uint32_t _us>
-lmcu_force_inline void us() { cyc((rcc::detail::system_clock / 1_MHz) * _us - 8); }
+static inline void ns(const uint64_t ns)
+{
+  uint32_t n = to_cycles<units::ns>(ns);
+  if(!__builtin_usubl_overflow(n, 8, &n)) { cyc(n); }
+}
 
 /**
- * @brief Dynamic delay in microseconds.
- * @param us: microseconds to wait
+ * @brief Delay in microseconds.
+ * @param _us: microsecond to wait
  *
- * @note  IMPORTANT: for max_delay see constant version.
+ * @note IMPORTANT: max_delay = (0xffffffff / f_cpu) * 1e6
 */
-lmcu_force_inline void us(const uint32_t us) { cyc((rcc::detail::system_clock / 1_MHz) * us - 8); }
+static inline void us(const uint32_t us) { ns(us * 1000); }
 
 /**
  * @brief Delay in millisecond.
  * @param ms: milliseconds to wait
 */
-lmcu_force_inline void ms(const uint32_t ms)
+static inline void ms(const uint32_t ms)
 {
-  for(uint32_t n = 0; n < ms; n++) { us<1000>(); }
+  for(uint32_t n = 0; n < ms; n++) { us(1000); }
 }
 
 /**
  * @brief Delay in seconds.
  * @param sec: seconds to wait
 */
-lmcu_force_inline void sec(const uint32_t sec)
+static inline void sec(const uint32_t sec)
 {
-  for(uint32_t n = 0; n < sec; n++) { us<1000000>(); }
+  for(uint32_t n = 0; n < sec; n++) { us(1000000); }
 }
 
 namespace dwt {
@@ -71,48 +57,39 @@ namespace dwt {
 class timer : public expirable
 {
 public:
-  enum units { cyc, us, ms, sec };
-
   template<units _units>
-  lmcu_force_inline void start(uint32_t t)
+  void start(uint32_t t)
   {
     if constexpr(_units == units::cyc) {
       expire_ = t;
     }
-    else
-    if constexpr(_units == units::us) {
-      expire_ = (rcc::detail::system_clock / 1_MHz) * t - 8;
-    }
-    else
-    if constexpr(_units == units::ms) {
-      expire_ = (rcc::detail::system_clock / 1_MHz) * t * 1000 - 8;
-    }
     else {
-      expire_ = (rcc::detail::system_clock / 1_MHz) * t * 1000000 - 8;
+      expire_ = to_cycles<_units>(t);
+      if(__builtin_usubl_overflow(expire_, 8, &expire_)) { expire_ = 0; }
     }
     start_ = delay::start();
   }
 
-  lmcu_force_inline bool expired() const final { return (DWT->CYCCNT + start_) >= expire_; }
+  inline bool expired() const final { return (DWT->CYCCNT + start_) >= expire_; }
 
   template<units _units>
-  lmcu_force_inline uint32_t elapsed() const
+  uint32_t elapsed() const
   {
     const uint32_t _cyc = DWT->CYCCNT + start_;
     if constexpr(_units == units::cyc) { return _cyc; }
-    const uint32_t _us = _cyc / (rcc::detail::system_clock / 1_MHz);
+    const uint32_t _us = _cyc / (rcc::system_clock() / 1_MHz);
     if constexpr(_units == units::us) { return _us;        }
     if constexpr(_units == units::ms) { return _us / 1000; }
     return _us / 1000000;
   }
 
   template<units _units>
-  lmcu_force_inline uint32_t remain() const
+  uint32_t remain() const
   {
     const uint32_t _cyc = DWT->CYCCNT + start_;
     if(_cyc >= expire_) { return 0; }
     if constexpr(_units == units::cyc) { return expire_ - _cyc; }
-    const uint32_t _us = (expire_ - _cyc) / (rcc::detail::system_clock / 1_MHz);
+    const uint32_t _us = (expire_ - _cyc) / (rcc::system_clock() / 1_MHz);
     if constexpr(_units == units::us) { return _us;        }
     if constexpr(_units == units::ms) { return _us / 1000; }
     return _us / 1000000;
