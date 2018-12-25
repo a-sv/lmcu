3138d68cb0ad9f64c03b111427c530f8abcdbc8c
a-sv <runtime.rnt@gmail.com>
2018-12-25 13:47:23 +0300

    added _GHz units

--- common/units.h
+++ common/units.h
@@ -3,10 +3,12 @@
 
 namespace lmcu {
 
-constexpr auto operator"" _MHz(long double x) { return static_cast<uint32_t>(x * 1e6); }
-constexpr auto operator"" _MHz(unsigned long long int x) { return static_cast<uint32_t>(x * 1e6); }
-constexpr auto operator"" _kHz(long double x) { return static_cast<uint32_t>(x * 1e3); }
-constexpr auto operator"" _kHz(unsigned long long int x) { return static_cast<uint32_t>(x * 1e3); }
+constexpr auto operator"" _GHz(long double x) { return static_cast<uint64_t>(x * 1e9L); }
+constexpr auto operator"" _GHz(unsigned long long int x) { return static_cast<uint64_t>(x * 1e9L); }
+constexpr auto operator"" _MHz(long double x) { return static_cast<uint64_t>(x * 1e6L); }
+constexpr auto operator"" _MHz(unsigned long long int x) { return static_cast<uint64_t>(x * 1e6L); }
+constexpr auto operator"" _kHz(long double x) { return static_cast<uint64_t>(x * 1e3L); }
+constexpr auto operator"" _kHz(unsigned long long int x) { return static_cast<uint64_t>(x * 1e3L); }
 
 constexpr auto operator"" _Mbyte(long double x) { return static_cast<uint64_t>(x * 1024 * 1024); }
 
