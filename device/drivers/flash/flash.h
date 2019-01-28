7a34979f14a1f1c4f1c707a575f736c33f15140c
a-sv <runtime.rnt@gmail.com>
2019-01-28 15:02:04 +0300

    drivers/flash: added 'is_nand' function

--- device/drivers/flash/flash.h
+++ device/drivers/flash/flash.h
@@ -10,4 +10,10 @@ enum class type
   nand_tlc
 };
 
+template<type _type>
+constexpr bool is_nand()
+{
+  return (_type == type::nand_slc) || (_type == type::nand_mlc) || (_type == type::nand_tlc);
+}
+
 } // namespace lmcu::drivers::flash
