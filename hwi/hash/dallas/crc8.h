36e2fe4c017d4029397ba8391dde3f1a0122dc3a
a-sv <runtime.rnt@gmail.com>
2019-06-27 12:03:09 +0300

    fix compile error

--- hwi/hash/dallas/crc8.h
+++ hwi/hash/dallas/crc8.h
@@ -7,25 +7,25 @@ class crc8
 {
 public:
   inline void init() { crc_ = 0; }
-  void update(const void *data, uint32_t sz);
+
+  void update(const void *data, uint32_t sz)
+  {
+    auto p = static_cast<const uint8_t*>(data), end = p + sz;
+
+    while(p < end) {
+      uint_fast8_t in = *p++;
+      for(uint_fast8_t i = 0; i < 8; ++i) {
+        uint_fast8_t mix = (crc_ ^ in) & 1U;
+        crc_ >>= 1U;
+        if(mix) { crc_ ^= 0x8CU; }
+        in >>= 1U;
+      }
+    }
+  }
+
   auto digest() const { return crc_; }
 private:
   uint_fast8_t crc_ = 0;
 };
-
-void crc8::update(const void *data, uint32_t sz)
-{
-  auto p = static_cast<const uint8_t*>(data), end = p + sz;
-
-  while(p < end) {
-    uint_fast8_t in = *p++;
-    for(uint_fast8_t i = 0; i < 8; ++i) {
-      uint_fast8_t mix = (crc_ ^ in) & 1U;
-      crc_ >>= 1U;
-      if(mix) { crc_ ^= 0x8CU; }
-      in >>= 1U;
-    }
-  }
-}
 
 } // namespace lmcu::hash::dallas
